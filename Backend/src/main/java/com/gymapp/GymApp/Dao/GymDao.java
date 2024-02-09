0c5e2ef24d1b18c19a69158b8cb93eea7ca6ddcb
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2024-02-09 11:30:56 -0300

    Nuevo pack
    Dao

---
+++ Backend/src/main/java/com/gymapp/GymApp/Dao/GymDao.java
@@ -0,0 +1,7 @@
+package com.gymapp.GymApp.Dao;
+import com.gymapp.GymApp.Entidades.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GymDao extends CrudRepository<Usuario, Long>{
+    
+}
