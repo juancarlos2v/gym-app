c4cd5d096af281034960122e3e4595ac8f77bc73
Lautaro <lauchagutierrez117@gmail.com>
2024-02-09 10:40:32 -0300

    build: creacion de proyecto gymapp

---
+++ Backend/src/test/java/com/gymapp/GymApp/GymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymapp.GymApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
