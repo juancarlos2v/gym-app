d7155ae7df9c4e55c51f85b9099693f4f88c81e0
Juan Carlos Vilcherrez <juanc.vilcherrez@gmail.com>
2024-02-13 16:57:30 -0300

    build: paquete de controladores-servicios

---
+++ Backend/src/main/java/com/gymapp/GymApp/Controladores/UsuarioControlador.java
@@ -0,0 +1,34 @@
+package com.gymapp.GymApp.Controladores;
+
+import com.gymapp.GymApp.Entidades.Usuario;
+import com.gymapp.GymApp.Servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioControlador {
+
+
+    /*
+     @Autowired
+    UsuarioServicio usuarioServicio;
+
+
+
+    @GetMapping("/registro")
+     public ResponseEntity<Object> registrar(){
+
+
+     return new ResponseEntity<>(HttpStatus.CREATED);
+
+     }*/
+
+
+
+
+}
