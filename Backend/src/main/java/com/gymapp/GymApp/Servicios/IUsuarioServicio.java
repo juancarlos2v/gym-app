08feaf346b0b36a44c9b2a52b7f9598d24050e8d
Agustina Maldonado <99924976+Agus1394@users.noreply.github.com>
2024-02-13 18:40:59 -0300

    agregacion de IUsuarioServicio, modificacion de UsuarioDao y creacion del metodo listarUsuarios

---
+++ Backend/src/main/java/com/gymapp/GymApp/Servicios/IUsuarioServicio.java
@@ -0,0 +1,12 @@
+package com.gymapp.GymApp.Servicios;
+
+import com.gymapp.GymApp.Entidades.Usuario;
+import java.util.List;
+
+public interface IUsuarioServicio {
+    
+    
+    public List<Usuario> traerUsuarios();
+    
+    
+}
--- Backend/src/main/java/com/gymapp/GymApp/Servicios/UsuarioServicio.java
+++ Backend/src/main/java/com/gymapp/GymApp/Servicios/UsuarioServicio.java
@@ -1,9 +1,22 @@
 package com.gymapp.GymApp.Servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.gymapp.GymApp.Dao.UsuarioDao;
+import com.gymapp.GymApp.Entidades.Usuario;
+import java.util.List;
 
 @Service
-public class UsuarioServicio {
+public class UsuarioServicio implements IUsuarioServicio {
 
+    @Autowired
+    private UsuarioDao usuarioDao;
 
+    @Override
+    public List<Usuario> traerUsuarios() {
+        List<Usuario> listaUsuarios = (List<Usuario>) usuarioDao.findAll();
+        return listaUsuarios;
+    }
+
+    
 }
