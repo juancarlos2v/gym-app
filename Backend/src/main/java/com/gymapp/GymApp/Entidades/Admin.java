82f8a0e97d83f5262b72cb847fdfb9e6e03fa414
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2024-02-14 11:47:54 -0300

    Revert "Merge branch 'oring/luciano' into backend"
    This reverts commit 15bebe5337c4bf06337e787175d6081da37c9d62, reversing
    changes made to 427533cd7f52e17e247978b518967168cfe16c26.

--- Backend/src/main/java/com/gymapp/GymApp/Entidades/Admin.java
+++ Backend/src/main/java/com/gymapp/GymApp/Entidades/Admin.java
@@ -1,39 +1,28 @@
 package com.gymapp.GymApp.Entidades;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
+
+@Entity
+@Table(name="Administrador")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
-@Table(name = "admin")
+@Getter
+@Setter
 public class Admin {
 
     @Id
-
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
-
-    @GeneratedValue(strategy = GenerationType.AUTO)
-
     private String id;
-
-
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idAdmin")
-    private Long idAdmin;
 
     private String nombre;
     private String password;
+
+
 
 }
