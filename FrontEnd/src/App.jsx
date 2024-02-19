c2bdcd217fdce4f279630e1bf079133d429a19cb
Mateo <brandonahumada804@gmail.com>
2024-02-19 10:06:33 -0300

    sideBar, tabla usuarios, y fromulario de creacion

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom"
+import Dashboard from './Component/Dashboard/Dashboard'
+import SideBar from './Component/SideBar/SideBar'
+import Users from './Component/Users/Users'
+import CreateUser from './Component/CreateUser/CreateUser'
+
 
 function App() {
 
   return (
     <>
-      <h1>hello word</h1>
+      <Routes>
+        <Route exact path='/' element={<CreateUser/>}/>
+        <Route exact path='/users' element= {<Users/>}/>
+      </Routes>
     </>
+      //  <Route exact path="/sideBar" element={<SideBar/>}/>
   )
 }
 
