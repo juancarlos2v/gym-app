c3b29f1b543f5e6b28abd4e5374c2007be761fe1
Juan Carlos Vilcherrez <juanc.vilcherrez@gmail.com>
2024-02-20 16:04:42 -0300

    fix: try react-java package

---
+++ Backend/src/frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    watch: {
+      include: "src/**",
+      exclude: "node_modules/**, .git/**, dist/**, .vscode/**",
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
