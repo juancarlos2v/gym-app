7f7d52dcf12c0975de1127e8111919aa7dab3865
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2024-03-01 22:57:03 -0300

    Revert "Revert "login""
    This reverts commit a066ce92fb066d8f76e8443625da318ffe3e0db3.

---
+++ src/main/java/com/gymapp/GymApp/Security/Config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.gymapp.GymApp.Security.Config;
+
+import com.gymapp.GymApp.Security.Jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf
+                        -> csrf
+                        .disable())
+                .authorizeHttpRequests(authRequest
+                        -> authRequest
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManager
+                        -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(withDefaults())
+                .build();
+    }
+}
